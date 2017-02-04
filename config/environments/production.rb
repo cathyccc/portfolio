2f338c7d0058cd227e4d080a9cea7c6ad23173bc
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-04 15:18:13 -0500

    changed background imageload to sass image-url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # config.serve_static_files = true
+  # Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
