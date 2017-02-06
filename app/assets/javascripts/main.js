7501933f5a2cba3461d6a0a3f415dd0ba39268d7
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-06 13:45:53 -0500

    changed typing effect source to fix bug

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,13 @@
 $(document).on('turbolinks:load',function(){
-  $(".text-typed").typed({
-      strings: ["coffee optimist.","board game enthusiast.","front-end developer looking to fit in the tech community."],
-      typeSpeed: 40
-  });
+  // $(".text-typed").typed({
+  //     strings: ["coffee optimist.","board game enthusiast.","front-end developer looking to fit in the tech community."],
+  //     typeSpeed: 40
+  // });
+  $(".text-typed").typeIt({
+     strings: ["coffee optimist.","board game enthusiast.","front-end developer looking to fit in the tech community."],
+     speed: 50,
+     breakLines: false,
+     autoStart: false
+});
 
 });
